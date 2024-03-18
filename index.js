9db78a299fa1596679f46179673864211481a158
TalitaGabriela <talitagabriela1216@gmail.com>
2024-03-18 11:44:09 -0400

    Commit
    aula remota

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
 const { Client } = require('pg');
 require("dotenv").config();
 
+const express = require("express");
+const app = express();
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+
+app.use(express.json());
+
+app.get("/", (req,res) => {
+    res.redirect("/usuarios/novo")
+})
+
+app.get("/usuarios/novo", (req,res) => {
+    res.sendFile(`${__dirname}/views/novo-usuario.html`)
+});
+
+app.post("/usuarios/novo" , (req,res) => {
+    const nickname = req.body.nickname;
+    const nome = req.body.nome;
+
+    client.query(`INSERT INTO usuarios (usuario_nickname, usuario_nome) 
+    VALUES ('${nickname}', '${nome}') returning *`, 
+    (err,result) => {
+        if(err){
+            res.send("Erro: " + err);
+        }else{
+            res.send("Sucesso, veja os dados: " + JSON.stringify(result.rows));
+        }
+    });
+});
+
+app.listen(8000, () => {
+    console.log("Servidor rodando na porta 8000.")
+});
+
 const client = new Client({
     user: process.env.USER,
     password: process.env.PASSWORD,
@@ -12,30 +50,30 @@ const client = new Client({
 client.connect()
 .then(() => {
     console.log("Conectado ao banco de dados PostgresSQL");
-    exibeUsuariosCadastrados()
+    //exibeUsuariosCadastrados()//
 })
 .catch((err)=>{
     console.log(`Erro: ${err}`)
 });
 
-function fechaConexao(){
-    client
-    .end()
-    .then(() => {
-        console.log("Conexão encerrada!");
-    })
-    .catch((err) => {
-        console.error("Erro ao encerrar conexão: " + err)
-    })
-}
+// function fechaConexao(){
+//     client
+//     .end()
+//     .then(() => {
+//         console.log("Conexão encerrada!");
+//     })
+//     .catch((err) => {
+//         console.error("Erro ao encerrar conexão: " + err)
+//     })
+// }
 
-function exibeUsuariosCadastrados(){
-    client.query("SELECT * FROM usuarios", (err,result) => {
-        if(err){
-            console.error("Erro ao executar a busca: " + err);
-        } else {
-            console.log("Resultado : " + JSON.stringify(result.rows));
-        }
-        fechaConexao()
-    });
-}
+// function exibeUsuariosCadastrados(){
+//     client.query("SELECT * FROM usuarios", (err,result) => {
+//         if(err){
+//             console.error("Erro ao executar a busca: " + err);
+//         } else {
+//             console.log("Resultado : " + JSON.stringify(result.rows));
+//         }
+//         fechaConexao()
+//     });
+// }
